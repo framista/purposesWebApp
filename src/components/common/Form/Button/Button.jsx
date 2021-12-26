9e2658762ce25374e86ff9de87dd092ac6905d14
framista <adajab1@gmail.com>
2021-12-26 21:22:02 +0100

    Add simple button

---
+++ src/components/common/Form/Button/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  const { onClick, disabled = false, loading = false, children } = props;
+
+  const handleClick = () => {
+    if (disabled || loading) return;
+    onClick();
+  };
+
+  return (
+    <button onClick={handleClick} aria-busy={loading} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+export default Button;
