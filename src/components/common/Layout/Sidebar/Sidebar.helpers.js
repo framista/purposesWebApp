6946cefe27a7ac941505f58fc205e3c16946b141
framista <adajab1@gmail.com>
2022-01-08 18:02:13 +0100

    Change queue for sidebar

--- src/components/common/Layout/Sidebar/Sidebar.helpers.js
+++ src/components/common/Layout/Sidebar/Sidebar.helpers.js
@@ -2,15 +2,15 @@ import { MdCategory, MdAreaChart } from 'react-icons/md';
 
 export const sidebarOptions = [
   {
+    id: 'dashboard',
+    destination: '/dashboard',
+    icon: <MdAreaChart />,
+    name: 'Dashboard',
+  },
+  {
     id: 'category',
     destination: '/category',
     icon: <MdCategory />,
     name: 'Categories',
-  },
-  {
-    id: 'dashboard',
-    destination: '/dashboard',
-    icon: <MdAreaChart />,
-    name: 'Dashboard'
   },
 ];
